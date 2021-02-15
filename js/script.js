b64eec0c8c12e2c7800ef7dab9c34c5490ed2b7d
robinhickson <rkhickson@gmail.com>
2021-02-15 12:24:15 +0100

    #1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+Treehouse Techdegree:
+FSJS Project 3 - Interactive Form
+*/
+/*jshint esversion: 8*/
+// GLOBAL VARIABLES //
+// ---------------------
+
+// Set the name input form field as focus true
+const getNameFocus = document.getElementById('name');
+getNameFocus.focus();
+
+//
