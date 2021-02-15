89f983e2967c0b03394b5f2df8ec08f07c694cc2
robinhickson <rkhickson@gmail.com>
2021-02-15 12:39:10 +0100

    #2

--- js/script.js
+++ js/script.js
@@ -5,9 +5,24 @@ FSJS Project 3 - Interactive Form
 /*jshint esversion: 8*/
 // GLOBAL VARIABLES //
 // ---------------------
+const getNameFocus = document.getElementById('name');
+const getOtherJobRole = document.getElementById('other-job-role');
+const getSelection = document.getElementById('title');
 
 // Set the name input form field as focus true
-const getNameFocus = document.getElementById('name');
 getNameFocus.focus();
+
+//Hide the job role option
+getOtherJobRole.style.display = "none";
+
+//Show and focus job role option input field if 'other' selected, hide otherwise
+getSelection.addEventListener('change', e => {
+if (e.target.value === 'other'){
+    getOtherJobRole.style.display = "inherit";
+    getOtherJobRole.focus();
+} else {
+    getOtherJobRole.style.display = "none";
+}
+});
 
 //
